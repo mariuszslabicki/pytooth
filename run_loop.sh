dc1a8a6e772e91a5c6292c57fd4f40adf796f7d5
Mariusz Slabicki <4083628+mariuszslabicki@users.noreply.github.com>
2020-08-21 11:34:52 +0000

    Update environment run in run_loop script

--- run_loop.sh
+++ run_loop.sh
@@ -9,18 +9,17 @@ fi
 
 rm $output_file
 
-pipenv shell
 first_run=true
-for advNo in 10 20 30
+for advNo in 10 20 30 40 50 60 70 80 90 100
 do
     for itNo in {0..9}
     do
         echo "Advno "$advNo" iteration " $itNo
         if [ "$first_run" = true ] ; then
-            python3 cmd.py --scNo 1 --advNo $advNo --simLen 100000000 --itNo $itNo >> $output_file
+            pipenv run python3 cmd.py --scNo 1 --advNo $advNo --simLen 100000000 --itNo $itNo >> $output_file
             first_run=false
         else
-            python3 cmd.py --scNo 1 --advNo $advNo --simLen 100000000 --itNo $itNo | sed -n '2 p' >> $output_file
+            pipenv run python3 cmd.py --scNo 1 --advNo $advNo --simLen 100000000 --itNo $itNo | sed -n '2 p' >> $output_file
         fi
     done
 done
